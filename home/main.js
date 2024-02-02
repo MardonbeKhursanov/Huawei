7c901457ecd8dfe0e4d0f49d8d7917361ef4d585
= <=>
2024-02-02 15:25:31 +0500

    tuzatildi

--- home/main.js
+++ home/main.js
@@ -1,6 +1,11 @@
 let corp = document.getElementById('corporate_id')
+let davlat = document.getElementById('davlat')
 
-
-corp.addEventListener('click', function(){
+corp.addEventListener("click", function(){
     document.querySelector('body').classList.toggle('corpo')
+    document.querySelector('body').classList.remove('tillar')
+})
+davlat.addEventListener('click', function(){
+    document.querySelector('body').classList.remove('corpo')
+    document.querySelector('body').classList.toggle('tillar')
 })
