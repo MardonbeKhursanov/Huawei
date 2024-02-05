3a1c3a778a2a08f0fcb6015226059a6134178c13
= <=>
2024-02-05 15:00:21 +0500

    Tuzatamiz

--- home/main.js
+++ home/main.js
@@ -1,5 +1,7 @@
 let corp = document.getElementById('corporate_id')
 let davlat = document.getElementById('davlat')
+let srch = document.getElementById('srch_btn')
+let x_srch = document.getElementById('x_srch')
 
 corp.addEventListener("click", function(){
     document.querySelector('body').classList.toggle('corpo')
@@ -8,4 +10,15 @@ corp.addEventListener("click", function(){
 davlat.addEventListener('click', function(){
     document.querySelector('body').classList.remove('corpo')
     document.querySelector('body').classList.toggle('tillar')
+})
+
+srch.addEventListener('click', function(){
+    let sr_bar = document.getElementById('search_bar').style.transform = 'scale(1)'
+    srch.style.transform = 'scale(0)'
+    document.getElementById('nav_ul').style.transform = 'scale(0)'
+})
+x_srch.addEventListener('click', function(){
+    let sr_bar = document.getElementById('search_bar').style.transform = 'scale(0)'
+    srch.style.transform = 'scale(1)'
+    document.getElementById('nav_ul').style.transform = 'scale(1)'
 })
